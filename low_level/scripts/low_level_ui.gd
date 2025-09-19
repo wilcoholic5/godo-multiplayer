05f6973fdfcc7f9f4c770e9758cd324a11a97f07
Cesar Flores <cesarflores.mi@gmail.com>
2025-09-19 16:14:49 -0700

    add text boxes

--- low_level/scripts/low_level_ui.gd
+++ low_level/scripts/low_level_ui.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_client_pressed() -> void:
-	LowLevelNetwork.start_client()
+	LowLevelNetwork.start_client($buttons/ip_address.text, int($buttons/port.text))
 
 func _on_server_pressed() -> void:
-	LowLevelNetwork.start_server()
+	LowLevelNetwork.start_server($buttons/ip_address.text, int($buttons/port.text))
